5fc1dcf482e4c96c390e492ba54e5b924bbe9d04
brendan <alber123@mail.chapman.edu>
2017-10-08 12:07:30 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/balbert/interactivestory/model/Choice.java
@@ -0,0 +1,31 @@
+package edu.orangecoastcollege.cs273.balbert.interactivestory.model;
+
+/**
+ * Created by brendantyleralbert on 9/29/17.
+ */
+
+public class Choice {
+    private int textId;
+    private int nextPage;
+
+    public Choice(int textId, int nextPage) {
+        this.textId = textId;
+        this.nextPage = nextPage;
+    }
+
+    public int getTextId() {
+        return textId;
+    }
+
+    public void setTextId(int textId) {
+        this.textId = textId;
+    }
+
+    public int getNextPage() {
+        return nextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        this.nextPage = nextPage;
+    }
+}
---
+++ app/src/main/java/edu/orangecoastcollege/cs273/balbert/interactivestory/ui/StoryActivity.java
@@ -0,0 +1,109 @@
+package edu.orangecoastcollege.cs273.balbert.interactivestory.ui;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Stack;
+
+import edu.orangecoastcollege.cs273.balbert.interactivestory.R;
+import edu.orangecoastcollege.cs273.balbert.interactivestory.model.Page;
+import edu.orangecoastcollege.cs273.balbert.interactivestory.model.Story;
+
+public class StoryActivity extends AppCompatActivity {
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private String name;
+    private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
+    private Stack<Integer> pageStack = new Stack<Integer>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        storyImageView = (ImageView) findViewById(R.id.storyImageView);
+        storyTextView = (TextView) findViewById(R.id.storyTextView);
+        choice1Button = (Button) findViewById(R.id.choice1Button);
+        choice2Button = (Button) findViewById(R.id.choice2Button);
+
+        Intent intent = getIntent();
+        name = intent.getStringExtra(getString(R.string.key_name));
+        if (name == null || name.isEmpty()) { name = "Friend"; }
+        Log.d(TAG, name);
+
+        story = new Story();
+        loadPage(0);
+    }
+
+    private void loadPage(int pageNumber) {
+        pageStack.push(pageNumber);
+
+        final Page page = story.getPage(pageNumber);
+
+        Drawable image = ContextCompat.getDrawable(this, page.getImageId());
+        storyImageView.setImageDrawable(image);
+
+        String pageText = getString(page.getTextId());
+        // Add name if placeholder included, won't add if not.
+        pageText = String.format(pageText, name);
+        storyTextView.setText(pageText);
+
+        if ( page.isFinalPage() ) {
+            choice1Button.setVisibility(View.INVISIBLE);
+            choice2Button.setText(R.string.play_again_button_text);
+            choice2Button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    loadPage(0);
+                }
+            });
+        }
+        else {
+            loadButtons(page);
+        }
+    }
+
+    private void loadButtons(final Page page) {
+        choice1Button.setVisibility(View.VISIBLE);
+        choice1Button.setText(page.getChoice1().getTextId());
+        choice1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nextPage = page.getChoice1().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+        choice2Button.setVisibility(View.VISIBLE);
+        choice2Button.setText(page.getChoice2().getTextId());
+        choice2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nextPage = page.getChoice2().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        pageStack.pop();
+        if (pageStack.isEmpty())
+            super.onBackPressed();
+        else
+            loadPage(pageStack.pop());
+    }
+}
